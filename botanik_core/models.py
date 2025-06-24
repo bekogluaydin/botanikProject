11bcc8918f0a283b1db6b9344d5e568275c1da50
Aydın BEKOĞLU <aydinbek97@gmail.com>
2025-06-24 16:41:08 +0300

    Add Collector model with phone number field and initial migration

--- botanik_core/models.py
+++ botanik_core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Collector(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    code = models.CharField(max_length=20, unique=True)
+    phone = PhoneNumberField(blank=True)
+    email = models.EmailField(blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
