ed8215b7f96bcd48d941bf27af3c9eebd8b18750
Aydın BEKOĞLU <aydinbek97@gmail.com>
2025-07-10 17:59:03 +0300

    Refactor permission checks to use has_permission_to_view and has_permission_to_add functions.

--- botanik_core/utils/permissions.py
+++ botanik_core/utils/permissions.py
@@ -1,35 +1,56 @@
 from django.http import HttpResponseForbidden
 from functools import wraps
 
-def user_can_view(table_name):
-    def decorator(view_func):
-        @wraps(view_func)
-        def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated:
-                return HttpResponseForbidden("Giriş yapmanız gerekiyor.")
+# Sayfaya erişmek için yetki kontrolü yapılıyor. Eğer görme, ekleme yetkisi yoksa uyarı verip belirtilen sayfaya yönlendirme yapıyor.
+def has_permission_to_view(user, table_name):
+    if user.is_superuser or user.is_staff:
+        return True
 
-            if hasattr(request.user, "userpermission"):
-                can_view = request.user.userpermission.can_view_tables.filter(name=table_name).exists()
-                if can_view:
-                    return view_func(request, *args, **kwargs)
-
-            return HttpResponseForbidden("Bu tabloyu görüntüleme yetkiniz yok.")
-        return _wrapped_view
-    return decorator
+    if not hasattr(user, "userpermission"):
+        return False
+    return user.userpermission.can_view_tables.filter(name=table_name).exists()
 
 
-def user_can_add(table_name):
-    def decorator(view_func):
-        @wraps(view_func)
-        def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated:
-                return HttpResponseForbidden("Giriş yapmanız gerekiyor.")
+def has_permission_to_add(user, table_name):
+    if user.is_superuser or user.is_staff:
+        return True
+    
+    if not hasattr(user, "userpermission"):
+        return False
+    return user.userpermission.can_add_tables.filter(name=table_name).exists()
 
-            if hasattr(request.user, "userpermission"):
-                can_add = request.user.userpermission.can_add_tables.filter(name=table_name).exists()
-                if can_add:
-                    return view_func(request, *args, **kwargs)
 
-            return HttpResponseForbidden("Bu tabloya kayıt yapma yetkiniz yok.")
-        return _wrapped_view
-    return decorator
+
+# Decorator ile sayfaya erişmek için yetki kontrolü yapılıyor. Eğer görme, ekleme yetkisi yoksa uyarı verip belirtilen sayfaya yönlendirme yapıyor.
+# def user_can_view(table_name):
+#     def decorator(view_func):
+#         @wraps(view_func)
+#         def _wrapped_view(request, *args, **kwargs):
+#             if not request.user.is_authenticated:
+#                 return HttpResponseForbidden("Giriş yapmanız gerekiyor.")
+
+#             if hasattr(request.user, "userpermission"):
+#                 can_view = request.user.userpermission.can_view_tables.filter(name=table_name).exists()
+#                 if can_view:
+#                     return view_func(request, *args, **kwargs)
+
+#             return HttpResponseForbidden("Bu tabloyu görüntüleme yetkiniz yok.")
+#         return _wrapped_view
+#     return decorator
+
+
+# def user_can_add(table_name):
+#     def decorator(view_func):
+#         @wraps(view_func)
+#         def _wrapped_view(request, *args, **kwargs):
+#             if not request.user.is_authenticated:
+#                 return HttpResponseForbidden("Giriş yapmanız gerekiyor.")
+
+#             if hasattr(request.user, "userpermission"):
+#                 can_add = request.user.userpermission.can_add_tables.filter(name=table_name).exists()
+#                 if can_add:
+#                     return view_func(request, *args, **kwargs)
+
+#             return HttpResponseForbidden("Bu tabloya kayıt yapma yetkiniz yok.")
+#         return _wrapped_view
+#     return decorator
